5c53d617f982e2588dd1f1d11f06e62cea9c1876
Shaheer Mir <shaheermir0@gmail.com>
2017-08-16 21:41:36 -0400

    Added flow to redux files

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,6 +1,8 @@
+// @flow
+
 import { SET_SEARCH_TERM } from './actions';
 
-export function setSearchTerm(searchTerm) {
+export function setSearchTerm(searchTerm: string) {
   return {
     type: SET_SEARCH_TERM,
     payload: searchTerm
