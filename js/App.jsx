334c8c3d7371239c2e484150530461543f032a4d
Shaheer Mir <shaheermir0@gmail.com>
2017-08-16 20:55:21 -0400

    Added Redux

--- js/App.jsx
+++ js/App.jsx
@@ -2,35 +2,39 @@
 
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import type { Match } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
 import preload from '../data.json';
+import store from './store';
 
 const FourOhFour = () => <h1>My FourOhFour Page!</h1>;
 
 const App = () => (
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route
-          path="/search"
-          component={props => <Search shows={preload.shows} {...props} />}
-        />
-        <Route
-          path="/details/:id"
-          component={(props: { match: Match }) => (
-            <Details
-              show={preload.shows.find(show => props.match.params.id === show.imdbID)}
-              {...props}
-            />
-          )}
-        />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route
+            path="/search"
+            component={props => <Search shows={preload.shows} {...props} />}
+          />
+          <Route
+            path="/details/:id"
+            component={(props: { match: Match }) => (
+              <Details
+                show={preload.shows.find(show => props.match.params.id === show.imdbID)}
+                {...props}
+              />
+            )}
+          />
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+    </Provider>
   </BrowserRouter>
 );
 
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,14 +1,49 @@
 // @flow
 
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import type { RouterHistory } from 'react-router-dom';
+import { setSearchTerm } from './actionCreators';
 
-const Landing = () => (
-  <div className="landing">
-    <h1>sVideo</h1>
-    <input type="text" placeholder="Search" />
-    <Link to="/search">or Browse All</Link>
-  </div>
-);
+class Landing extends React.Component {
+  props: {
+    searchTerm: string,
+    handleSearchTermChange: Function,
+    history: RouterHistory
+  };
 
-export default Landing;
+  goToSearch = (e: SyntheticEvent) => {
+    e.preventDefault();
+    this.props.history.push('/search');
+  };
+
+  render() {
+    return (
+      <div className="landing">
+        <h1>sVideo</h1>
+        <form onSubmit={this.goToSearch}>
+          <input
+            type="text"
+            placeholder="Search"
+            value={this.props.searchTerm}
+            onChange={this.props.handleSearchTermChange}
+          />
+        </form>
+        <Link to="/search">or Browse All</Link>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleSearchTermChange(event) {
+    dispatch(setSearchTerm(event.target.value));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
